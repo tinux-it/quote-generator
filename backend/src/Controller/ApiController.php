241b212769dc6ddab5117db0424f892bfbd437bd
Tom Emming <tom@tinux-it.nl>
2025-04-21 14:55:35 +0200

    feat: reworked the assertions and made sure that all requests are valid

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -4,28 +4,41 @@ namespace App\Controller;
 
 use App\DTO\SubscribeRequest;
 use App\DTO\UnSubscribeRequest;
+use App\Handler\SubscribeHandler;
+use App\Handler\SubscriptionHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api')]
 final class ApiController extends AbstractController
 {
     public function __construct(
-        private readonly SerializerInterface $serializer
+        private readonly ValidatorInterface $validator,
+        private readonly SerializerInterface $serializer,
+        private readonly SubscriptionHandler $subscriptionHandler,
     ) {
     }
-
 
     #[Route('/subscribe', methods: ['GET', 'POST'])]
     public function subscribe(Request $request): JsonResponse
     {
         $content = $request->getContent();
         $subscribeRequest = $this->serializer->deserialize($content, SubscribeRequest::class, 'json');
+        $errors = $this->validateAndReturnErrors($subscribeRequest);
+        if ($errors) {
+            return $errors;
+        }
 
-        return new JsonResponse($subscribeRequest);
+        $message = $this->subscriptionHandler->subscribe($subscribeRequest);
+        return new JsonResponse([
+            "message" => $message,
+            "status" => Response::HTTP_OK,
+        ]);
     }
 
     #[Route('/unsubscribe', methods: ['POST'])]
@@ -33,7 +46,34 @@ final class ApiController extends AbstractController
     {
         $content = $request->getContent();
         $unSubscribeRequest = $this->serializer->deserialize($content, UnSubscribeRequest::class, 'json');
+        $errors = $this->validateAndReturnErrors($unSubscribeRequest);
+        if ($errors) {
+            return $errors;
+        }
 
-        return new JsonResponse($unSubscribeRequest);
+        $message = $this->subscriptionHandler->unSubscribe($unSubscribeRequest);
+        return new JsonResponse([
+            "message" => $message,
+            "status" => Response::HTTP_OK,
+        ]);
     }
+
+    private function validateAndReturnErrors(object $requestDTO): ?JsonResponse
+    {
+        $errors = $this->validator->validate($requestDTO);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+
+            return new JsonResponse(
+                ['errors' => $errorMessages],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        return null;
+    }
+
 }
--- backend/src/DTO/SubscribeRequest.php
+++ backend/src/DTO/SubscribeRequest.php
@@ -10,9 +10,23 @@ final class SubscribeRequest
         message: 'The email "{{ value }}" is not a valid email.')]
     public string $email;
 
-    #[Assert\Length(min: 10, max: 10, minMessage: 'The phone number "{{ value }}" is not valid.', maxMessage: 'The phone number "{{ value }}" is not valid.')]
+    #[Assert\Length(
+        min: 10,
+        max: 10,
+        minMessage: 'The phone number "{{ value }}" is not valid.',
+        maxMessage: 'The phone number "{{ value }}" is not valid.'
+    )]
+    #[Assert\When(
+        expression: 'this.phoneNumber !== null',
+        constraints: [
+            new Assert\NotBlank(message: 'If provided, the phone number cannot be blank')
+        ]
+    )]
     public string $phoneNumber;
 
-    #[Assert\Choice(choices: ['email', 'whatsapp', 'browser'])]
+    #[Assert\NotBlank]
+    #[Assert\All([
+        new Assert\Choice(choices: ["email", "whatsapp", "browser"])
+    ])]
     public array $methods;
 }
