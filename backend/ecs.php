6aee5df4f84f00dfb7078bf6f8fe891138a19825
Tom Emming <tom@tinux-it.nl>
2025-04-21 20:10:14 +0200

    feat: added ecs.php file

---
+++ backend/ecs.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocAlignFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src'
+    ]);
+
+    // Skip files
+    $ecsConfig->skip([
+        __DIR__ . '/src/Migrations',
+    ]);
+
+    // Import PSR-12 rules
+    $ecsConfig->sets([
+        SetList::PSR_12,
+        SetList::CLEAN_CODE,
+        SetList::ARRAY,
+    ]);
+
+    // Configure specific rules
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(OrderedImportsFixer::class, [
+        'imports_order' => ['class', 'function', 'const'],
+    ]);
+
+    $ecsConfig->rules([
+        DeclareStrictTypesFixer::class,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(OrderedClassElementsFixer::class, [
+        'order' => ['use_trait', 'constant_public', 'constant_protected', 'constant_private', 'property_public', 'property_protected', 'property_private', 'construct', 'method_public', 'method_protected', 'method_private'],
+    ]);
+};
