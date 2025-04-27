1cec1826a59966b8ac398dcbd2100c7a2545e0bc
Tom Emming <tom@tinux-it.nl>
2025-04-27 13:01:05 +0200

    chore: update the hard-coded API url

--- frontend/src/app/service/SubscriptionService.ts
+++ frontend/src/app/service/SubscriptionService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_URL || 'http://api.quote-gen.tomemming.nl/api';
+const API_BASE_URL = process.env.API_URL || 'http://api.quote.tomemming.nl/api';
 
 // Update the interface to match the new backend requirements
 export interface SubscriptionData {
